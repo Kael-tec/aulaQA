be3433683e02ca82da0cff7b904912eda754292e
KaeltecQA <nascimentopereiracesarkael@gmail.com>
2024-11-19 17:26:46 -0300

    tuplaLisDicSet

---
+++ tuplaListaDicSet.py
@@ -0,0 +1,22 @@
+# Lista é uma coleção ordenada e mutável. Permite membros duplicados.
+lista = ["carro", True, 2, 3.5]
+print(lista)
+print (type(lista) )
+print ("-"*30)
+
+# Tupla é uma coleção ordenada e imutável. Permite membros duplicados.
+tupla = ("carro", True, 2, 3.5)
+print (tupla)
+print (type(tupla) )
+print("_"*30)
+
+# O dicionário é uma coleção ordenada e mutável. Nenhum membro duplicado.
+dicionario = {"nome": "carro", "logica": True, "numero": 2, "outroNumero": 3.5}
+print(dicionario)
+print (type(dicionario) )
+print ("_"*30)
+
+# Set é uma coleção não ordenada e não indexada. Nenhum membro duplicado.
+conjunto = {"carro", True, 2, 3.5}
+print (conjunto)
+print (type(conjunto) )
